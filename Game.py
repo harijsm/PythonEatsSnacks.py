68673d25a9a3ae7a039a7a265d91f007b35835f6
harijs.me <harijs@mendo.lv>
2014-03-07 17:35:40 +0200

    * display score * display speed in % of start speed * snake gets fat while eating snacks

--- Game.py
+++ Game.py
@@ -13,6 +13,7 @@ class Game:
         self.height = height
         self.width = width
         self.run = True
+        self.startSpeed = self.speed
         self.Main()
     def CreateNewSnake(self):
         'Creates new Snake object'
@@ -51,13 +52,18 @@ class Game:
             board[k][self.width-1] = "="
 
         board[self.foodx][self.foody] = "F"
-        board[self.snake.x][self.snake.y] = "X"
+        for i in self.snake.positions:
+            board[i[0]][i[1]] = "X"
         for i in board:
             print " ".join(i)
+        print ''
+        currSpeed = (self.startSpeed/self.speed)*100
+        print ' SCORE: %s ' % (self.score)
+        print ' SPEED: ' + str(int(currSpeed)) + '%'
     def Collision(self):
         if self.snake.x == self.foodx and self.snake.y == self.foody:
             self.score += 1
-            self.speed += -0.05
+            self.speed += -0.025
             self.snake.size += 1
             self.CreateNewFoodItem()
         if self.snake.x == self.height-2 or self.snake.y == self.width-2 or self.snake.x == 0 or self.snake.y == 0:
@@ -65,7 +71,7 @@ class Game:
 
             os.system('clear')
             print ''
-            print ''
+            print ' --- SCORE: %s --- ' % (self.score)
             print ' === GAME OVER === '
             print ''
             print ''
@@ -82,8 +88,9 @@ class Snake:
     def __init__(self, x=0, y=0, size=1, direction='up'):
         self.x = x
         self.y = y
-        self.size = size
+        self.size = size+1
         self.direction = direction
+        self.positions = []
     def Move(self):
         'Changes snakes x, y according to its direction'
         if self.direction == 'up':
@@ -94,6 +101,10 @@ class Snake:
             self.y = self.y-1
         elif self.direction == 'right':
             self.y = self.y+1
+
+        self.positions.append([self.x, self.y])
+        del self.positions[len(self.positions)-self.size:len(self.positions)-self.size+1]
+
     def ChangeDirection(self, key):
         'Changes direction the snake is moving'
         keys = {
